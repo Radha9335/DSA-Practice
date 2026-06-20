596d18dd12c7423698be35e4e6680c80a7899fc6
Radha jha <radha.jha2004@gmail.com>
2026-06-20 11:37:36 +0530

    Leetcode 540

---
+++ Binary Search/Medium/SearchSingleElementInAsortedArray.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int n=nums.size();
+        int low=0;
+        int high=n-1;
+
+        while(low<high){
+            int mid=low+(high-low)/2;
+
+            if(mid%2==0){
+                if(nums[mid]==nums[mid+1]){
+                    low=mid+1;
+                }
+                else{
+                    high=mid;
+                }
+            }
+            else{
+                if(nums[mid]==nums[mid-1]){
+                    low=mid+1;
+                }
+                else{
+                    high=mid-1;
+                }
+            }
+        }
+        return nums[low];
+    }
+};
