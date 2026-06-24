a46bb6b6dc80f85507b83a90cdfe59ff0822a551
Radha jha <radha.jha2004@gmail.com>
2026-06-24 11:53:20 +0530

    FloorAndCeilInSortedArray

--- Binary Search/Easy/FloorAndCeilInSortedArray.cpp
+++ Binary Search/Easy/FloorAndCeilInSortedArray.cpp
@@ -2,4 +2,36 @@
 using namespace std;
 
 
+class Solution {
+public:
+    vector<int> getFloorAndCeil(vector<int> nums, int x) {
+   int n=nums.size();
+   vector<int>v;
+        int low=0;
+        int high=n-1;
+        int ans=-1;
+        int ans2=-1;
+       
+
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(nums[mid]==x){
+                return {x,x};
+            }
+           else if(nums[mid]<=x){
+                ans=nums[mid];
+                low=mid+1;
+            }
+            else{
+                ans2=nums[mid];
+                high=mid-1;
+            }
+        }
+        return {ans,ans2};
+
+    }
+};
+
+
+
 
--- Binary Search/Easy/FoorInASortedArray.cpp
+++ Binary Search/Easy/FoorInASortedArray.cpp
@@ -3,13 +3,15 @@ using namespace std;
 
 
 class Solution {
-public:
-    vector<int> getFloorAndCeil(vector<int> nums, int x) {
-   int n=nums.size();
+  public:
+    int findFloor(vector<int>& nums, int x) {
+        // code here
+        int n=nums.size();
         int low=0;
         int high=n-1;
         int ans=-1;
         if(nums[0]>x) return -1;
+        
         while(low<=high){
             int mid=(low+high)/2;
             
