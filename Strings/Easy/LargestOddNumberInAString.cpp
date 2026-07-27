c91722b3a06a2045bba7c7035b60eb4e71cfd0ed
Radha jha <radha.jha2004@gmail.com>
2026-07-27 13:59:27 +0530

    leetcode 1903

---
+++ Strings/Easy/LargestOddNumberInAString.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string largestOddNumber(string num) {
+        
+        int n=num.length();
+    
+
+        for(int i=n-1;i>=0;i--){
+            int r=(int)num[i]-'0';
+            if(r%2!=0){
+                return num.substr(0,i+1);
+            }
+            
+        }
+      return "";
+    }
+};
